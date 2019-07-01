3470513fb8722327775c9592643e824e5017fe09
J-Kyu <wjstkdrb91@naver.com>
2019-07-02 01:02:51 +0900

    no message

---
+++ Assets/Scripts/EndGameText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameText : MonoBehaviour
+{
+    [SerializeField] private Text finalScore = null;
+    [SerializeField] private Ground ground = null;
+
+    // Start is called before the first frame update
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        string finalString = string.Format("End Game\nFinal Score: {0:F}",ground.GetRadius());
+        finalScore.text = finalString;
+    }
+}
